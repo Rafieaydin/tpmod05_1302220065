5f947eda2832a81965c8f37b8e261bb1cc613249
Rafieaydin <rafie.aydin1@gmail.com>
2024-03-17 20:00:34 +0700

    remove file

--- TPMOD05/DataGeneric/DataGeneric/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using HaloGeneric;
-class Program {
-    public static void Main(string[] args)
-    {
-        HaloGeneric<String> data = new HaloGeneric<String>("1302220065");
-        data.PrintData();
-    }
-}
